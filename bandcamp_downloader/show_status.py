c33f5edc2bfd78cfdcc9be7a85ce71dd9faff482
sentriz <senan.f.b.kelly+github@gmail.com>
2014-08-12 03:51:41 +0100

    user agent settings and improvemnts

---
+++ bandcamp_downloader/show_status.py
@@ -0,0 +1,49 @@
+from colorama import Fore, Style, init
+init(autoreset = True)
+
+last_message = None
+
+def show_status(message = "", status = "", once_off = False):
+    global last_message
+    
+    def replace_text(string):
+        colours = {"%" + color.lower() + "%": getattr(Fore, color) \
+            for color in dir(Fore) if "_" not in color}
+        styles = {"%" + style.lower() + "%": getattr(Style, style) \
+            for style in dir(Style) if "_" not in style}
+        for alias, esc in colours.items() | styles.items():
+            string = string.replace(alias, esc)
+        return string
+    
+    message = replace_text(message)
+    status = replace_text(status)
+    
+    if not once_off:
+        print(message + "... ", end = "")
+        if last_message:
+            print("\r" + last_message + "... " + status)
+            last_message = None
+        else:
+            last_message = message
+    else:
+        print(message + replace_text("%yellow% .....%reset%"))
+        
+# testing
+if __name__ == "__main__":
+    import time
+    
+    show_status("doing thing")
+    time.sleep(.1)
+    show_status(status = "%green%done")
+    time.sleep(.1)
+    show_status("doing something else")
+    time.sleep(.1)
+    show_status(status = "%red%failed")
+    time.sleep(.1)
+    show_status("doing weird thing", once_off = True)
+    time.sleep(.1)
+    show_status("doing weird thing again", once_off = True)
+    time.sleep(.1)
+    show_status("doing something else, will faileds")
+    time.sleep(.1)
+    show_status(status = "%red%faileds")
